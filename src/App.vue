bafae0036de3a2169d81ceaf084423f7cf91a12b
Nombre prueba <prueba@ejemplo.com>
2026-01-14 11:29:56 +0100

    Creo homepage

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+
+import HomePage from '@/components/HomePage.vue'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <HomePage />
+  <RouterView />
 </template>
 
 <style scoped></style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="hero-wrapper">
+        <header class="navbar">
+        <div class="container">
+            <div class="brand">Meal Planner</div>
+            <nav class="nav-links">
+            <router-link to="./MyMeal.vue" class="nav-item">Lista de platos</router-link>
+            <router-link to="/plan" class="nav-item">Plan semanal</router-link>
+            </nav>
+        </div>
+        </header>
+
+        <section class="hero">
+        <img 
+            src="https://img.freepik.com/free-photo/delicious-gourmet-meal-copy-space_23-2148517060.jpg?semt=ais_hybrid&w=1920&q=80" 
+            alt="Deliciosa comida gourmet" 
+            class="hero-image"/>
+        <div class="hero-overlay">
+            <h2>¿Qué cocinamos hoy?</h2>
+        </div>
+        </section>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
